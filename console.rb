e7c6eec9340bf8b28f7e098507ac180908ed7723
junko911 <junkotahara911@gmail.com>
2020-07-20 11:42:53 -0400

    Commennted out test cases in console.rb

--- console.rb
+++ console.rb
@@ -3,19 +3,20 @@ require_relative './book'
 require_relative './author'
 require_relative './author_book'
 
-author1 = Author.new
-author2 = Author.new
-author3 = Author.new
-author4 = Author.new
-author5 = Author.new
-author6 = Author.new
+#Test cases
+# author1 = Author.new
+# author2 = Author.new
+# author3 = Author.new
+# author4 = Author.new
+# author5 = Author.new
+# author6 = Author.new
 
-book1 = author1.write_book("Spring", 100)
-book2 = author2.write_book("Summer",200 )
-book3 = author3.write_book("Autumn", 300)
-book4 = author4.write_book("Winter", 400)
-book5 = author5.write_book("Year", 500)
-book6 = author6.write_book("Time", 600)
+# book1 = author1.write_book("Spring", 100)
+# book2 = author2.write_book("Summer",200 )
+# book3 = author3.write_book("Autumn", 300)
+# book4 = author4.write_book("Winter", 400)
+# book5 = author5.write_book("Year", 500)
+# book6 = author6.write_book("Time", 600)
 
 
 binding.pry
