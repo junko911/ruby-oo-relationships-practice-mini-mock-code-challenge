1dfc7d121775b9d95f8f4b08b5a6fcf809deffac
junko911 <junkotahara911@gmail.com>
2020-07-20 09:24:34 -0400

    Created self.all methods for Author and Book class.

--- author.rb
+++ author.rb
@@ -1,3 +1,11 @@
 class Author
-  
+  @@all = []
+
+  def initialize
+    Author.all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
--- book.rb
+++ book.rb
@@ -1,7 +1,13 @@
 class Book
   attr_reader :title
+  @@all = []
 
   def initialize(title)
     @title = title
+    Book.all << self
+  end
+
+  def self.all
+    @@all
   end
 end
